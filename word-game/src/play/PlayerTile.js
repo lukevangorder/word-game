6ff0b5de958f1c4e3c5d1630d1e15c5b033f463a
lukevangorder <lukevangorder@gmail.com>
2021-06-03 10:03:04 -0400

    Adds event listener to PlayerTile compoonents to highlight selected tile

--- word-game/src/play/PlayerTile.js
+++ word-game/src/play/PlayerTile.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 
 export default class PlayerTile extends Component {
 
+    constructor(){
+        super();
+        this.state = {
+           highlight: false
+        }
+    }
+
+    handleClick = () => {
+        this.setState({
+            highlight: !this.state.highlight
+        })
+    }
+
     render() {
+        let tileClass = this.state.highlight ? "highlight" : "playertile"
         return (
-            <div class='playertile'>
+            <div class={tileClass} onClick={this.handleClick}>
                 
             </div>
         )
