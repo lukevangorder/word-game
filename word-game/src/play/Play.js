20af91c71a13132ea8608b4c84a89fc31130de88
lukevangorder <lukevangorder@gmail.com>
2021-06-03 08:49:15 -0400

    Adds play and board classes in src/play

---
+++ word-game/src/play/Play.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Board from './Board.js'
+
+export default class Play extends Component {
+    render() {
+        return (
+            <Board />
+        )
+    }
+}
