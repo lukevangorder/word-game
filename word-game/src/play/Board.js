2ed2a174441805a2086dd1e9fd0c967822983647
lukevangorder <lukevangorder@gmail.com>
2021-06-03 08:59:48 -0400

    Adds boardtile class and css class

--- word-game/src/play/Board.js
+++ word-game/src/play/Board.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import BoardTile from './BoardTile.js'
 
-export default class Play extends Component {
+export default class Board extends Component {
     render() {
         return (
             <div class='board'>
-                
+                <BoardTile />
+                <BoardTile />
+                <BoardTile />
+                <BoardTile />
+                <BoardTile />
+                <BoardTile />
+                <BoardTile />
+                <BoardTile />
+                <BoardTile />
+                <BoardTile />
+                <BoardTile />
+                <BoardTile />
+                <BoardTile />
+                <BoardTile />
+                <BoardTile />
+                <BoardTile />
+                <BoardTile />
+                <BoardTile />
             </div>
         )
     }
