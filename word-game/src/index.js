0891d4fa5fc835ce063942f506f7fdd1aea6d4c2
lukevangorder <lukevangorder@gmail.com>
2021-06-03 12:13:06 -0400

    Adds chessSetReducer.js in reducers directory and exports to index

--- word-game/src/index.js
+++ word-game/src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import chessSetReducer from './reducers/chessSetReducer.js'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(chessSetReducer)
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
-);
+); 
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- word-game/src/play/BoardTile.js
+++ word-game/src/play/BoardTile.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import GamePiece from './GamePiece.js';
 
 export default class BoardTile extends Component {
     render() {
