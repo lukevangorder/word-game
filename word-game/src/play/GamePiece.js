5e3bcb19ea9a2212c5aec22d0fffddfac68364f2
lukevangorder <lukevangorder@gmail.com>
2021-06-03 18:57:39 -0400

    Adds event listener to GamePiece elements

--- word-game/src/play/GamePiece.js
+++ word-game/src/play/GamePiece.js
@@ -2,27 +2,32 @@ import React, { Component } from 'react';
 
 export default class GamePiece extends Component {
 
-    constructor() {
+    constructor(props) {
         super()
         this.state = {
-            // col: props.col,
-            // row: props.row,
-            // pieceImage: props.pieceImage,
-            // color: props.color,
-            // captured: props.captured,
-            // type: props.type
+            col: props.col,
+            row: props.row,
+            pieceImage: props.pieceImage,
+            color: props.color,
+            captured: props.captured,
+            type: props.type
         }
     }
 
-    //Moving one row or column linear is an 80px change, starting at the bottom left hand corner with a px of 320 left and 640 top
+    clickHandler = () => {
+        switch(this.type){
+            case 'pawn':
+                
+        }
+    }
 
     render() {
-        const lefty = ((this.props.col - 1)*80);
-        const toppy = ((this.props.row - 1)*80);
+        const lefty = ((this.state.col - 1)*80);
+        const toppy = ((this.state.row - 1)*80);
         const styleAlter = {left:lefty + 'px', top:toppy + 'px'}
         return (
-            <div class='gamepiece' style={styleAlter}>
-                {this.props.pieceImage}
+            <div class='gamepiece' style={styleAlter} onClick={this.clickHandler}>
+                {this.state.pieceImage}
             </div>
         )
     }
