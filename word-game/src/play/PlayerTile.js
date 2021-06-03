2fbca0d604cfe2e6940178f478f69365f1fe0219
lukevangorder <lukevangorder@gmail.com>
2021-06-03 09:51:35 -0400

    Adds PlayerTile component and css class and exports to TileBar.js

---
+++ word-game/src/play/PlayerTile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class PlayerTile extends Component {
+
+    render() {
+        return (
+            <div class='playertile'>
+                
+            </div>
+        )
+    }
+}
--- word-game/src/play/TileBar.js
+++ word-game/src/play/TileBar.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import PlayerTile from './PlayerTile.js'
 
 export default class TileBar extends Component {
 
     render() {
         return (
             <div class='tilebar'>
-                
+                <PlayerTile />
+                <PlayerTile />
+                <PlayerTile />
+                <PlayerTile />
+                <PlayerTile />
+                <PlayerTile />
+                <PlayerTile />
             </div>
         )
     }
